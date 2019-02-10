dfa9d87f45f4486cdff0df42c1bbf472a06e64af
lianino <lianino@mail.ru>
2019-02-10 21:33:18 +0700

    Первое создание

---
+++ lessons_Csharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace lessons_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello World!!!");
+
+            Console.WriteLine("Привет мир!!!");
+
+
+            string data;
+
+            data = Console.ReadLine();
+
+
+            Console.WriteLine("Привет " + data + "!!!");*/
+
+            string str = "5";
+
+            int a = Convert.ToInt32(str);
+
+            string str2 = "2";
+
+            int b = Convert.ToInt32(str2);
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
+
